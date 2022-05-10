1ff8ec329134be54fd68be929fc1ea4f6eba1eca
Tim Vogler <tim.vogler@fau.de>
2022-05-10 20:14:19 +0200

    Create basic backend structure

---
+++ FleetMgmnt/main.py
@@ -0,0 +1,26 @@
+import threading
+import time
+
+# Internal dependencies
+import webserver
+
+
+def main():
+    launch_thread(webserver.start, ())
+    launch_thread(placeholder, ())  # Example
+
+
+def placeholder():
+    while 1:
+        time.sleep(50000)
+
+
+def launch_thread(target_function, args):
+    thread = threading.Thread(target=target_function, args=args)
+    thread.start()
+
+
+if __name__ == '__main__':
+    main()
+
+
