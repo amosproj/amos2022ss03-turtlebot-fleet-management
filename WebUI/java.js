6f737688b262520eac5ae9362c6798fa7f74b4be
Tim Vogler <tim@allianceapps.de>
2022-05-10 13:48:36 +0200

    Install Axios JS library (REST client)

--- WebUI/java.js
+++ WebUI/java.js
@@ -1,9 +1,23 @@
 const { createApp } = Vue
 
+
 createApp({
     data() {
         return {
             message: 'Hello Vue!'
         }
-    }
+    },
 }).mount('#app')
+
+axios.get('/hans')
+    .then(function (response) {
+        // handle success
+        console.log(response);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+    });
