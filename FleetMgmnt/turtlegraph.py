16dba46fa9c7b80a1eaf706929f61eb2f6b56daa
Tim Vogler <tim.vogler@fau.de>
2022-05-23 14:31:12 +0200

    Convert Vmap Line/Points into Turtlegraph

--- FleetMgmnt/turtlegraph.py
+++ FleetMgmnt/turtlegraph.py
@@ -1,10 +1,17 @@
-import typing
-from typing import List
+import io
+import math
+
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+import vmap_importer
 
 
 class Node:
-    def __init__(self, nid: int, description: str = None):
+    def __init__(self, nid: int, x: float, y: float,  description: str = None):
         self.nid = nid
+        self.x = x
+        self.y = y
         self.description = description
 
 
@@ -22,8 +29,19 @@ class Graph:
         self.node_id = 0
         self.edge_id = 0
 
-    def new_node(self):
-        node = Node(self.node_id)
+    def vmap_lines_to_graph(self, file: str):
+        points, lines = vmap_importer.import_vmap(file)
+        print(len(points))
+        print(len(lines))
+        for i, point in enumerate(points):
+            self.new_node(point.x, point.y, point.name)
+        for line in lines:
+            start = self.find_node_by_coords(line.start.x, line.start.y)
+            end = self.find_node_by_coords(line.end.x, line.end.y)
+            self.new_edge(start, end, math.dist(line.start.get_coords(), line.end.get_coords()))
+
+    def new_node(self, x: float, y: float, description: str):
+        node = Node(self.node_id, x, y, description)
         self.node_id += 1
         self.nodes.append(node)
         return node
@@ -33,6 +51,12 @@ class Graph:
         self.edge_id += 1
         self.edges.append(n_edge)
         return n_edge
+
+    def find_node_by_coords(self, x: float, y: float):
+        for node in self.nodes:
+            if node.x == x and node.y == y:
+                return node
+        raise Exception("Node not found, FATAL")
 
     def is_strongly_connected(self):
         # Todo
@@ -47,57 +71,12 @@ class Graph:
                 node_edges.append(e)
         return node_edges
 
-
-
-
-
-
-graph = Graph()
-
-# Playground
-
-node1 = graph.new_node()
-node2 = graph.new_node()
-node3 = graph.new_node()
-node4 = graph.new_node()
-
-edge1 = graph.new_edge(node1, node3, 15)
-edge2 = graph.new_edge(node3, node4, 13)
-edge3 = graph.new_edge(node4, node2, 2)
-edge4 = graph.new_edge(node1, node2, 8)
-edge5 = graph.new_edge(node1, node4, 87)
-edge6 = graph.new_edge(node2, node3, 9)
-
-
-
-print(graph.nodes)
-print(graph.edges)
-print(graph.get_node_edges(node1))
-
-for edge in graph.get_node_edges(node1):
-    print("Can reach " + str(edge.end.nid))
-
-# graph = [node1, node2, node3, node4]
-# visited = list()
-# q = list()
-# q.append(node1)
-# visited.append(node1)
-#
-# while len(q) > 0:
-#     cur = q.pop()
-#     print(cur.nid)
-#
-#     for edge in cur.edges:
-#         if edge.start == cur:
-#             if edge.end in visited:
-#                 continue
-#             visited.append(edge.end)
-#             q.append(edge.end)
-#         else:
-#             if edge.start in visited:
-#                 continue
-#             visited.append(edge.start)
-#             q.append(edge.start)
-#
-#
+    def create_image(self):
+        plt_io = io.BytesIO()
+        for edge in self.edges:
+            plt.plot([edge.start.x, edge.end.x], [edge.start.y, edge.end.y], linestyle='dashed', marker='s')
+        for node in self.nodes:
+            plt.annotate(node.description, (node.x, node.y))
+        plt.savefig(plt_io, format='png', dpi=300)
+        return plt_io
 
