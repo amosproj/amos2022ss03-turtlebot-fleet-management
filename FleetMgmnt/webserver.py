40465a15f4bfe3cf78648ce5b88c803339938776
Tim Vogler <tim.vogler@fau.de>
2022-05-23 14:32:13 +0200

    Output Turtlegraph image in webserver

--- FleetMgmnt/webserver.py
+++ FleetMgmnt/webserver.py
@@ -1,10 +1,23 @@
-from flask import Flask, send_from_directory
+import io
+
+from flask import Flask, send_from_directory, Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+import turtlegraph
 
 app = Flask(__name__)
 
 
 def start():
     app.run(host='0.0.0.0', port=8080)
+
+
+@app.route("/graph")
+def graph_image():
+    graph = turtlegraph.Graph()
+    graph.vmap_lines_to_graph("demo.vmap")
+    img = graph.create_image()
+    return Response(img.getvalue(), mimetype='image/png')
 
 
 @app.route("/")
