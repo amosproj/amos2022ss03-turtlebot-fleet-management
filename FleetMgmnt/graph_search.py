0d957c2891a7fa7e8745f07e12d9989556b1c308
Niklas Markert <niklas.markert@fau.de>
2022-05-30 11:26:02 +0200

    Fixed some PEP8 guidelines

--- FleetMgmnt/graph_search.py
+++ FleetMgmnt/graph_search.py
@@ -1,7 +1,8 @@
-# https://github.com/jrialland/python-astar
-from astar import AStar
-import turtlegraph
 import math
+
+import turtlegraph
+from astar import AStar  # https://github.com/jrialland/python-astar
+
 
 class GraphSearch(AStar):
 
@@ -9,13 +10,13 @@ class GraphSearch(AStar):
         self.graph = graph
 
     def neighbors(self, node):
-        ''' For a given node, returns the list of its neighbors. '''
+        """ For a given node, returns the list of its neighbors. """
         edges = self.graph.get_node_edges(node)
         neighbors = [e.end for e in edges]
         return neighbors
     
     def distance_between(self, n1, n2):
-        ''' Gives the real distance/cost between two adjacent nodes n1 and n2. '''
+        """ Gives the real distance/cost between two adjacent nodes n1 and n2. """
         edges = self.graph.get_node_edges(n1)
         for e in edges:
             if e.end.nid == n2.nid:
@@ -24,11 +25,11 @@ class GraphSearch(AStar):
         return dist
 
     def heuristic_cost_estimate(self, current, goal):
-        ''' Computes the estimated distance/cost between a node and the goal. '''
+        """ Computes the estimated distance/cost between a node and the goal. """
         return math.sqrt((current.x - goal.x)**2 + (current.y - goal.y)**2)
 
     def is_goal_reached(self, current, goal):
-        ''' This method shall return a truthy value when the goal is 'reached'. '''
+        """ This method shall return a truthy value when the goal is 'reached'. """
         return current.nid == goal.nid
 
 
