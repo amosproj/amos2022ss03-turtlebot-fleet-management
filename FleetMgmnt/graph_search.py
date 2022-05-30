e5d11382f3ff776fe6cbac94fc8c54619c5d6ce7
Niklas Markert <niklas.markert@fau.de>
2022-05-30 14:32:15 +0200

    Added GraphSearch class for storing already calculated shortest routes (#59)
    Signed-off-by: Niklas Markert <niklas.markert@fau.de>

--- FleetMgmnt/graph_search.py
+++ FleetMgmnt/graph_search.py
@@ -4,18 +4,18 @@ import turtlegraph
 from astar import AStar  # https://github.com/jrialland/python-astar
 
 
-class GraphSearch(AStar):
+class AStarSearch(AStar):
 
-    def __init__(self, graph):
+    def __init__(self, graph: turtlegraph.Graph):
         self.graph = graph
 
-    def neighbors(self, node):
+    def neighbors(self, node: turtlegraph.Node):
         """ For a given node, returns the list of its neighbors. """
         edges = self.graph.get_node_edges(node)
         neighbors = [e.end for e in edges]
         return neighbors
-    
-    def distance_between(self, n1, n2):
+
+    def distance_between(self, n1: turtlegraph.Node, n2: turtlegraph.Node):
         """ Gives the real distance/cost between two adjacent nodes n1 and n2. """
         edges = self.graph.get_node_edges(n1)
         for e in edges:
@@ -24,23 +24,46 @@ class GraphSearch(AStar):
                 break
         return dist
 
-    def heuristic_cost_estimate(self, current, goal):
+    def heuristic_cost_estimate(self, current: turtlegraph.Node, goal: turtlegraph.Node):
         """ Computes the estimated distance/cost between a node and the goal. """
-        return math.sqrt((current.x - goal.x)**2 + (current.y - goal.y)**2)
+        return math.sqrt((current.x - goal.x) ** 2 + (current.y - goal.y) ** 2)
 
-    def is_goal_reached(self, current, goal):
+    def is_goal_reached(self, current: turtlegraph.Node, goal: turtlegraph.Node):
         """ This method shall return a truthy value when the goal is 'reached'. """
         return current.nid == goal.nid
 
 
+class GraphSearch:
+
+    def __init__(self, graph: turtlegraph.Graph):
+        self.graph = graph
+        self.a_star_search = AStarSearch(graph)
+        self.shortest_routes = dict()  # Stores the shortest route for already calculated pairs of (start, end)
+
+    def get_shortest_route(self, start: turtlegraph.Node, end: turtlegraph.Node):
+        key = (start.nid, end.nid)
+        if key not in self.shortest_routes.keys():
+            self.shortest_routes[key] = self.calculate_shortest_route(start, end)
+        return self.shortest_routes[key]
+
+    def calculate_shortest_route(self, start: turtlegraph.Node, end: turtlegraph.Node):
+        route_nodes = list(self.a_star_search.astar(start, end))  # List of nodes
+        route_edges = list()  # List of edges
+        for i, node in enumerate(route_nodes[:-1]):
+            edges = self.graph.get_node_edges(node)
+            for edge in edges:
+                if edge.end.nid == route_nodes[i + 1].nid:
+                    route_edges.append(edge)
+                    continue
+        return route_nodes, route_edges
+
+
 if __name__ == '__main__':
-    graph = turtlegraph.Graph()
-    graph.vmap_lines_to_graph("Test1.vmap")
+    g = turtlegraph.Graph()
+    g.vmap_lines_to_graph("demo.vmap")
 
-    start = graph.nodes[3]
-    end = graph.nodes[0]
-    found_path = list(GraphSearch(graph).astar(start, end))
+    gs = GraphSearch(g)
+    nodes, edges = gs.get_shortest_route(g.nodes[2], g.nodes[17])
 
-    print('Start:', start.nid, '- End:', end.nid)
-    for n in found_path:
-        print(n.nid)
+    for edge in edges:
+        print(edge.start.nid, '->', edge.end.nid)
