a99b4ae42d7356750efd4aef1c7f16bdb54b553a
Sebastian2023 <92729241+Sebastian2023@users.noreply.github.com>
2022-06-15 06:55:26 +0200

    Improved driver
    to be able to perform non rectangular turns and follow the line between nodes.
    
    Co-authored-by meike.bloecher@fau.de
    Co-authored-by niklas.markert@outlook.de

--- TurtleBot/dev_ws/worker_node/worker_node/worker.py
+++ TurtleBot/dev_ws/worker_node/worker_node/worker.py
@@ -10,7 +10,7 @@ from sensor_msgs.msg import BatteryState as ROSBatteryState
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
 from sick_lidar_localization.msg import *
-from vda5050_msgs.msg import AGVPosition, BatteryState, Order, OrderInformation, Visualization
+from vda5050_msgs.msg import AGVPosition, BatteryState, Connection, Order, OrderInformation, Visualization
 
 class Worker(Node):
     # subscriber callbacks
@@ -26,6 +26,8 @@ class Worker(Node):
     # Sick
     def call_location(self, msg):
         # self.get_logger().info('Receiving "%s"' %msg)
+        self.position_initialized = True if msg.loc_status > 30 else False
+        self.localization_score = (100.0 - msg.loc_status) / 100
         # x, y in m, heading in deg
         self.pose = [float(msg.x / 1000), float(msg.y / 1000), float(msg.heading / 1000)]
 
@@ -38,6 +40,7 @@ class Worker(Node):
         self.battery_charge = msg.percentage
         self.battery_voltage = msg.voltage
 
+
     def __init__(self):
         super().__init__('worker')
 
@@ -46,20 +49,25 @@ class Worker(Node):
         # vars
         # MQTT
         self.data = ""
-        self.order_ids = []
-        self.orderUpdateId = 0
 
         # Sick
+        self.position_initialized = False
+        self.localization_score = 0.0
         self.pose = [0.0, 0.0, 0.0]
         self.num_lanes = 0
         self.lanes = []
 
+        self.driving = False
         self.orders_ids = []
+        self.order_id = ""
         self.orders_nodes = []
+        self.last_node_id = ""
+        self.last_node_sequence_id = 0
 
         # Kuboki
         self.battery_charge = 0.0
         self.battery_voltage = 0.0
+
 
         # subscribe to
         # MQTT
@@ -71,16 +79,38 @@ class Worker(Node):
         # Kuboki
         self.battery_sub = self.create_subscription(ROSBatteryState, "/mobile_base/sensors/battery_state", self.call_batterystate, 10)
 
+
         # publish to
         # MQTT
         self.pub_mqtt = self.create_publisher(String, "/echo", 10)
         self.pub_state = self.create_publisher(OrderInformation, "/state", 10)
         self.pub_visualisation = self.create_publisher(Visualization, "/visualization", 10)
+        self.pub_connection = self.create_publisher(Connection, "/connection", 10)
+
+        # say hello
+        msg_connection = Connection()
+        msg_connection.connection_state = "ONLINE"
+        self.pub_connection.publish(msg_connection)
+        # cself.get_logger().info('Publishing: "%s"' %msg_connection)
 
         # Kuboki
         self.pub_power = self.create_publisher(MotorPower, "/mobile_base/commands/motor_power", 10)
         self.pub_vel = self.create_publisher(Twist, "/mobile_base/commands/velocity", 10)
 
+        # switch on motor power
+        msg_power = MotorPower()
+        msg_power.state = 1
+        self.pub_power.publish(msg_power)
+        # self.get_logger().info('Publishing: "%s"' %msg_power)
+
+        # demo move
+        #msg_twist = Twist()
+        #msg_twist.linear.x = 0.1
+        #msg_twist.angular.z = 0.8
+        # self.pub_vel.publish(msg_twist)
+
+
+        # create independent timers
         timer_period = 1
         self.group1 = MutuallyExclusiveCallbackGroup()
         self.group2 = MutuallyExclusiveCallbackGroup()
@@ -88,22 +118,19 @@ class Worker(Node):
         self.timer2 = self.create_timer(timer_period, self.controller_callback, self.group2)
         self.i = 0
 
+    def deinit(self):
+        msg_connection = Connection()
+        msg_connection.connection_state = "OFFLINE"
+        self.pub_connection.publish(msg_connection)
+        # self.get_logger().info('Publishing: "%s"' %msg_connection)
+
     # timer callbacks
+    # publisher
     def publisher_callback(self):
-        # switch on power for motor
-        msg_power = MotorPower()
-        msg_power.state = 1
-        self.pub_power.publish(msg_power)
-        # self.get_logger().info('Publishing: "%s"' %msg_power)
-
-        # demo move
-        # msg_twist = Twist()
-        # msg_twist.linear.x = 0.1
-        # msg_twist.angular.z = 0.8
-        # self.pub_vel.publish(msg_twist)
-
-        # publish position and battery to FMS
+        # publish battery state and position to FMS
         msg_agvpos = AGVPosition()
+        msg_agvpos.position_initialized = self.position_initialized
+        msg_agvpos.localization_score = self.localization_score
         # x, y in m, thea in [-pi ... pi]
         msg_agvpos.x = self.pose[0]
         msg_agvpos.y = self.pose[1]
@@ -114,8 +141,13 @@ class Worker(Node):
         msg_batterystate.battery_voltage = self.battery_voltage
 
         msg_state = OrderInformation()
+        msg_state.order_id = self.order_id
+        msg_state.last_node_id = self.last_node_id
+        msg_state.last_node_sequence_id = self.last_node_sequence_id
+        msg_state.driving = self.driving
         msg_state.agv_position = msg_agvpos
         msg_state.battery_state = msg_batterystate
+        self.pub_state.publish(msg_state)
         # self.get_logger().info('Publishing: "%s"' %msg_state)
 
         # optional
@@ -124,133 +156,130 @@ class Worker(Node):
         # msg_vis.agv_position = msg_agvpos
         # self.pub_visualisation.publish(msg_vis)
         # self.get_logger().info('Publishing: "%s"' %msg_vis)
-
         self.i += 1
 
-    # maps negative poses to 0...+360
+
+    # helpers for controller
+    # map negative angles to positive ones
     def normalize_pose(self, pose):
         if pose < 0:
-            return 360 - abs(self.pose[2])
+            return 360 - abs(pose)
         return pose
 
-    # rotates by a given angle
+    # rotate turtlebot by given angle
     def rotate_self(self, angle):
         msg_twist = Twist()
+        speed = 0.4
+        # pose normalization
         pose = self.normalize_pose(self.pose[2])
-
-        # some slack
-        lower_angle = ((angle + pose) % 360) - 5
-        upper_angle = (((angle + pose) % 360) + 5) % 360
-
-        self.get_logger().info('enter loop')
+        angle = self.normalize_pose(angle)
+        # rotate smaller direction
+        if ((angle - pose) + 360) % 360 > 180:
+            speed = -speed
+        # add acceptable margin
+        lower_angle =  int(angle - 3)
+        if lower_angle < 0:
+            lower_angle = 360 + lower_angle
+        upper_angle = int((angle + 3) % 360)
+        angle_list = range(lower_angle, upper_angle + 1)
+        if lower_angle > upper_angle:
+            angle_list = list(range(lower_angle, 360)) + list(range(0, upper_angle + 1))
+        # print out debug info
+        self.get_logger().info('enter loop turn')
         self.get_logger().info('pose "%f"' %pose)
         self.get_logger().info('angle "%f"' %angle)
-        self.get_logger().info('lower_angle "%f"' %lower_angle)
-        self.get_logger().info('upper_angle "%f"' %upper_angle)
-
-        # TODO check if turning in the other direction is faster
+        self.get_logger().info('list lower "%d"' %angle_list[0])
+        self.get_logger().info('list_upper "%d"' %angle_list[-1])
         # turn until target angle reached
-        while(pose > upper_angle or pose < lower_angle):
+        while(int(pose) not in angle_list):
             pose = self.normalize_pose(self.pose[2])
-            self.get_logger().info('currently "%d"' %pose)
-            msg_twist.angular.z = 0.2
+            # self.get_logger().info('currently "%d"' %pose)
+            msg_twist.angular.z = speed
             self.pub_vel.publish(msg_twist)
-            time.sleep(0.1)
-
+            time.sleep(0.2)
+        self.get_logger().info('pose "%d"' %pose)
         self.get_logger().info('leave')
-
         msg_twist.angular.z = 0.0
         self.pub_vel.publish(msg_twist)
 
-    # follows virtual line
+    # follow line
     def follow_line(self, goal):
         msg_twist = Twist()
-
-        # some slack
-        lower_goal_x = goal.node_position.x * 0.7
-        lower_goal_y = goal.node_position.y * 0.7
-        upper_goal_x = goal.node_position.x * 1.3
-        upper_goal_y = goal.node_position.y * 1.3
-
-        self.get_logger().info('enter loop')
-
+        last_lane = False
+        self.get_logger().info('enter loop line')
         while True:
             # calculate x and y d from goal
             dx = abs(self.pose[0] - goal.node_position.x)
             dy = abs(self.pose[1] - goal.node_position.y)
-
             # check if goal reached
-            if(dx < 0.25 and dy < 0.25):
+            if(dx < 0.2 and dy < 0.2):
                break
-
-            self.get_logger().info('px = "%f"' %self.pose[0])
-            self.get_logger().info('py = "%f"' %self.pose[1])
-            self.get_logger().info('dx = "%f"' %(upper_goal_x - self.pose[0]))
-            self.get_logger().info('dy = "%f"' %(upper_goal_y - self.pose[1]))
-
-            # drive straight
-            msg_twist.linear.x = 0.2
+            # print debug info
+            # self.get_logger().info('px = "%f"' %self.pose[0])
+            # self.get_logger().info('py = "%f"' %self.pose[1])
+            # self.get_logger().info('dx = "%f"' %dx)
+            # self.get_logger().info('dy = "%f"' %dy)
+            msg_twist.linear.x = 0.3
             self.pub_vel.publish(msg_twist)
-
-            # TODO use virtual line sensor
-            # if self.lane_num != 0:
-            #    if self.lanes[0] > 15:
-            #        TODO relative turn
-            #    elif self.lanes[0] < -15:
-            #        ...
-
+            # use virtual line sensor
+            # check if robot was on line in last iteration
+            if self.num_lanes == 0:
+                last_lane = False
+            # use virtual line information if available and not too close to intersection
+            if self.num_lanes != 0 and (dx > 0.5 or dy > 0.5):
+                self.get_logger().info('lane "%d"' %self.num_lanes)
+                num = 0
+                if self.num_lanes >= 1:
+                    num = self.lanes.index(min(self.lanes))
+                self.get_logger().info('lane_num = "%d"' %num)
+                # right margin
+                if self.lanes[num] > 40 and last_lane == True:
+                    self.get_logger().info('turn left')
+                    self.rotate_self(self.pose[2] + 5)
+                # left margin
+                elif self.lanes[num] < -40 and last_lane == True:
+                    self.get_logger().info('turn right')
+                    self.rotate_self(self.pose[2] - 5)
+                last_lane = True
             time.sleep(0.3)
-
         self.get_logger().info('leave')
-
         msg_twist.linear.x = 0.0
         self.pub_vel.publish(msg_twist)
 
     def controller_callback(self):
         # onshot only
         self.timer2.cancel()
-
         while True:
-            self.get_logger().info('Take new order')
+            finished_orders = []
             for order in self.orders_nodes or []:
-                self.get_logger().info('Got to next node')
+                self.get_logger().info('Go to next node')
+                self.order_id = order.order_id
                 for node in order or []:
-                    # calculate distance to next node
                     # TODO make it work with arbitrary coord systems
+                    pose = self.normalize_pose(self.pose[2])
+                    # compute driving direction
                     dx = node.node_position.x - self.pose[0]
                     dy = node.node_position.y - self.pose[1]
-
+                    direction = math.degrees(math.atan2(dy, dx))
+                    # print out debug info
+                    self.get_logger().info('d "%d"' %direction)
                     self.get_logger().info('dx "%s"' %dx)
                     self.get_logger().info('dy "%s"' %dy)
-
-                    # calculate direction of next node
-                    pose = self.normalize_pose(self.pose[2])
-                    direction = math.degrees(math.atan2(dy, dx))
-
-                    self.get_logger().info('d "%d"' %direction)
-
-                    # rectangular turn
-                    #if direction < 135 and direction >= 45:
-                        # turn right 90 deg
-                        #self.get_logger().info('Rotating right')
-                        #self.rotate_self(90)
-                    #elif direction >= 135 and direction < 215:
-                        # turn 180 deg
-                        #self.get_logger().info('Turning around')
-                        #self.rotate_self(180)
-                    #elif direction >= 215 and direction < 305:
-                        # turn left 270 deg
-                        #self.get_logger().info('Rotating left')
-                        #self.rotate_self(270)
-                    #elif direction >= 305 or direction < 45:
-                        # drive straight
-                        #self.get_logger().info('Straight')
-
-                    # turn robot to calculated direction
-                    self.rotate_self(direction - pose)
+                    # rotate in driving direction
+                    self.driving = True
+                    self.get_logger().info('rotate')
+                    self.rotate_self(direction)
+                    # follow line until next node
                     self.get_logger().info('Following line')
                     self.follow_line(node)
-                return
+                    self.driving = False
+                    self.last_node_id = node.node_id
+                    self.last_node_sequence_id = node.last_node_sequence_id
+                # when one order is finished move it to finished_orders
+                finished_orders.append(order)
+            # when all orders are finished, remove the finished orders
+            for order in finished_orders:
+                self.orders_nodes.remove(order)
 
 
 def main(args=None):
@@ -259,8 +288,15 @@ def main(args=None):
     worker = Worker()
     executor = MultiThreadedExecutor(num_threads=2)
     executor.add_node(worker)
-    executor.spin()
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        pass
+    except ExternalShutdownException:
+        pass
+    finally:
+        worker.deinit()
+
 
 if __name__ == '__main__':
     main()
-
