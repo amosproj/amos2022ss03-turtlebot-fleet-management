f9de9d1480f671960415bca1692b6f1b625d60ea
Venkatesh770 <venkatesh.kumar.moorthy@fau.de>
2022-07-17 19:40:52 +0200

    Implemented Feature #124 withour order path highlight

--- FleetMgmnt/WebUI/java.js
+++ FleetMgmnt/WebUI/java.js
@@ -11,14 +11,31 @@ createApp({
             toStation: null,
             agvs: [],
             orders: [],
+            graph : [],
+            orderpath : [],
+            agv_pos_color : [],
         }
     },
     methods: {
         refreshMap() {
-            document.getElementById('graphmap').src = 'graph?' + Math.random()
+           document.getElementById('graphmap').src = 'graph?' + Math.random()
         },
         async sendOrder() {
             await axios.post('/api/agv/' + this.robotSerial + '/sendFromTo/' + this.fromStation + '/' + this.toStation)
+            const graph_node_order_promise = axios.get('/api/agv/' + this.robotSerial + '/coordinate_pathDisplay/' + this.fromStation + '/' + this.toStation)
+            this.orderpath = (await graph_node_order_promise).data
+                        for(let i=0;i<this.orderpath.length;i++){
+                     myLineChart.data.datasets.push({
+                        label: 'Order_'+ i ,
+                        data: this.orderpath[i],
+                        fill: false,
+                        showLine: true,
+                        borderColor: 'red',
+                        tension: 0.1,
+                        order: 0
+                        });
+                 };
+                 window.myLineChart.update();
         },
         async cancelOrder(id) {
             await axios.delete('/api/orders/' + id)
@@ -33,10 +50,108 @@ createApp({
             this.refreshMap()
 
             const orders_promise = axios.get('/api/orders')
-            const agv_states_promise = axios.get('/api/agvs')
+            const agv_states_promise = axios.get('/api/agv')
+            const graph_node_promise = axios.get('/api/graph/coordinates')
+            const agv_coordinate_color = axios.get('/api/graph/agv_coordinates')
 
             this.orders = (await orders_promise).data
             this.agvs = (await agv_states_promise).data
+            this.graph = (await graph_node_promise).data
+            this.agv_pos_color = (await agv_coordinate_color).data
+
+            console.log(this.agv_pos_color)
+
+            const points = new Array()
+            window.myLineChart = new Chart('ChartJsChart', {
+                type: 'scatter',
+                data: {
+                datasets: [{
+                label: "Test-Graph",
+                fill: false,
+                borderColor: "gray",
+                showLine: false,
+                data: this.graph[0] ,
+                        }]
+                         },
+                options: {
+                          plugins:{
+                         autocolors: false,
+                          annotation: {
+                            annotations:points
+                          },
+                       legend: {
+                            display: false
+                             }
+                              },
+                       scales: {
+                        x: {
+                            grid: {
+                                display: false
+                        }
+                      },
+                        y: {
+                            grid: {
+                                display: false
+                        }
+                      }
+                    }
+                       },
+                    plugins: ['chartjs-plugin-annotation']
+                    });
+            for(let i=0;i<this.graph.length;i++){
+                     myLineChart.data.datasets.push({
+                        label: 'Edge_'+ i ,
+                        data: this.graph[i],
+                        fill: false,
+                        showLine: true,
+                        borderColor: 'gray',
+                        tension: 0.1,
+                        order: 1
+                        });
+                 };
+            for(let i=0;i<this.stations.length;i++){
+                     points.push({
+                          type: 'point',
+                          backgroundColor: 'red',
+                          borderColor: 'blue',
+                          borderWidth: 1,
+                          pointStyle: 'rectRot',
+                          scaleID: 'y',
+                          xValue: this.stations[i].x,
+                          yValue: this.stations[i].y
+                        });
+                 };
+                 for(let i=0;i<this.stations.length;i++){
+                     points.push({
+                          type: 'label',
+                          borderColor: 'green',
+                          borderRadius: 7,
+                          borderWidth: 2,
+                          content: [this.stations[i].name+' ('+this.stations[i].nid+')'],
+                          font: {
+                            size:16
+                          },
+                          position: {
+                            x: 'end',
+                            y: 'end'
+                          },
+                          xValue: this.stations[i].x,
+                          yValue: this.stations[i].y
+                        });
+                                      };
+                 for(let i=0;i<this.agv_pos_color.length;i++){
+                     points.push({
+                          type: 'point',
+                          backgroundColor: this.agv_pos_color[i].color,
+                          borderColor: 'blue',
+                          borderWidth: 1,
+                          pointStyle: 'rectRot',
+                          scaleID: 'y',
+                          xValue: this.agv_pos_color[i].x,
+                          yValue: this.agv_pos_color[i].y
+                        });
+                 };
+            window.myLineChart.update();
         }
     },
     created() {
