850a9d1f31786db5fc13ec60e137adf2fd475286
Sebastian2023 <92729241+Sebastian2023@users.noreply.github.com>
2022-07-26 00:06:05 +0200

    Cleaned up turtlebot code
    Co-authored-by: MeikeBloecher 104436432+MeikeBloecher@users.noreply.github.com

--- TurtleBot/dev_ws/worker_node/launch/amos.launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ros2 launch turtlebot2_ros2 bringup.launch.py > /dev/null 2>&1 &
-ros2 run sick_lidar_localization sick_lidar_localization src/sick_lidar_localization/launch/sick_lidar_localization.launch > /dev/null 2>&1 &
-ros2 run worker_node worker
--- TurtleBot/dev_ws/worker_node/worker_node/map_client.py
+++ TurtleBot/dev_ws/worker_node/worker_node/map_client.py
@@ -8,34 +8,39 @@ from rclpy.action import ActionClient
 from rclpy.node import Node
 
 
+# service client for setting the map in SICK LiDARLoc
 class MinimalMapClientSet(Node):
 
     def __init__(self):
-        super().__init__('minimal_client_async')
+        super().__init__('minimal_map_client_set')
         self.cli = self.create_client(LocSetMapSrv, 'LocSetMap')
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('map set service not available, waiting again...')
         self.req = LocSetMapSrv.Request()
+        self.future = None
 
     def send_request(self, path):
         self.req.mappath = str(path)
         self.future = self.cli.call_async(self.req)
 
 
+# service client for loading the newly set map into SICK LiDARLoc
 class MinimalMapClientLoad(Node):
 
     def __init__(self):
-        super().__init__('minimal_client_set_async')
+        super().__init__('minimal_map_client_load')
         self.cli = self.create_client(LocLoadMapToCacheSrv, 'LocLoadMapToCache')
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('map load service not available, waiting again...')
         self.req = LocLoadMapToCacheSrv.Request()
+        self.future = None
 
     def send_request(self, path):
         self.req.mappath = str(path)
         self.future = self.cli.call_async(self.req)
 
 
+# action client for starting auto docking of kobuki robot
 class DockingClient(Node):
 
     def __init__(self):
